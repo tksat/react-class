c16b54b222f0f3d7425be3c69d65bc5dfddd6054
tksat <satoko.takada@findy.co.jp>
2021-01-04 23:24:23 +0900

    giphyのgifを imageを取得

---
+++ src/GiphyApp.js
@@ -0,0 +1,45 @@
+import React from "react"
+import axios from "axios"
+
+class GiphyApp extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { gifs: [] }
+  }
+
+  getGif = () => {
+    const searchArray = {
+      text: "cat",
+      limit: 10,
+      apiKey: "QgQ8FCmQXZ1u6nLkFFLmPcf1Pn3aokWp"
+    }
+    const url = `http://api.giphy.com/v1/gifs/search?q=${searchArray.text}&api_key=${searchArray.apiKey}&limit=${searchArray.limit}`
+
+    axios.get(url).then(res => {
+      const dataObj = res.data.data
+      const imageUrlList = dataObj.map(data => data.images.downsized.url)
+      this.setState({ gifs: imageUrlList })
+    })
+  }
+
+  imageList = (urlList, index) => {
+    const item = urlList.map(url => <li key={index}><img src={url} /></li>)
+    console.log(urlList)
+    return <ul>{item}</ul>
+  }
+
+  componentDidMount() {
+    this.getGif()
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Giphyを検索アプリ</h1>
+        {this.imageList(this.state.gifs)}
+      </>
+    )
+  }
+}
+
+export default GiphyApp
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import App from "./App"
+import GiphyApp from "./GiphyApp"
 
-render(<App />, document.getElementById("root"))
+render(<GiphyApp />, document.getElementById("root"))
