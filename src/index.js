1d1a88525b4df602b881c5727e7fc28e826a8c02
tksat <satoko.takada@findy.co.jp>
2021-01-11 12:18:44 +0900

    初期baseの準備

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from "react"
 import { render } from 'react-dom';
-import { createStore, applyMiddleware } from "redux"
-import rootReducer from "./reducers"
-import { Provider } from "react-redux"
-import CountApp from "./containers/CountApp"
-import thunk from "redux-thunk"
-
-const middlewares = [thunk]
-
-const store = createStore(rootReducer, applyMiddleware(...middlewares))
+import GiphyApp from './component/GiphyApp'
 
 render(
-  <Provider store={store}>
-    <CountApp />
-  </Provider>
+  <>
+    <GiphyApp />
+  </>
   , document.getElementById("root"))
--- src/reducers/day.js
+++
@@ -1,8 +0,0 @@
-const day = (state = "2021年1月", action) => {
-  switch (action.type) {
-    default:
-      return state
-  }
-}
-
-export default day
