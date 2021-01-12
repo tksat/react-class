75e46392a2742070dc97218c2dd167cfbf7b86ea
tksat <satoko.takada@findy.co.jp>
2021-01-12 14:19:14 +0900

    検索コンポーネントの作成

--- src/component/GiphyApp.js
+++ src/component/GiphyApp.js
@@ -3,12 +3,14 @@ import { createStore } from "redux"
 import { Provider } from "react-redux"
 import rootReducer from "../reducers"
 import ImageList from "../containers/ImageList"
+import Serch from "./Serch"
 
 const store = createStore(rootReducer)
 
 const GiphyApp = () => {
   return (
     <Provider store={store}>
+      <Serch />
       <ImageList />
     </Provider >
   )
---
+++ src/component/Serch.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+class Serch extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { text: "" }
+  }
+
+  handleOnChange = e => {
+    this.setState({ text: e.target.value })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    this.setState({ text: "" })
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" value={this.state.text} onChange={e => this.handleOnChange(e)} />
+        <input type="submit" />
+      </form>
+    )
+  }
+}
+
+export default Serch
